1ece06d784017017e588da1e0d9a5c5a4d37dd59
gemstoneyang <bennyws99@gmail.com>
2022-05-08 19:49:10 +0900

    구글 로그인 연동

--- src/main/java/com/hello/book/web/IndexController.java
+++ src/main/java/com/hello/book/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hello.book.web;
 
+import com.hello.book.config.auth.dto.SessionUser;
 import com.hello.book.service.posts.PostsService;
 import com.hello.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("loginUserName", user.getName());
+        }
+
         return "index";
     }
 
