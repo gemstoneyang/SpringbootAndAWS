3f0f8f2123283660a13f2de3b657a25faa12ed01
gemstoneyang <bennyws99@gmail.com>
2022-05-10 22:55:59 +0900

    무중단 배포 오류 수정 5

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,12 +4,12 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-IDLE_PORT=${find_idle_port}
+IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
-if [[ -z ${IDLE_PID} ]];
+if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
@@ -17,4 +17,3 @@ else
   kill -15 ${IDLE_PID}
   sleep 5
 fi
-
