e6fb9555bc9d348ec1630733e130ad03288df20e
gemstoneyang <bennyws99@gmail.com>
2022-05-10 23:04:44 +0900

    무중단 배포 오류 수정 6

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/evn bash
+#!/usr/bin/env bash
 
 function find_idle_profile() {
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if [ ${RESPONSE_CODE} -ge 400 ]
+  if [[ ${RESPONSE_CODE} -ge 400 ]];
   then
     CURRENT_PROFILE=real2
   else
     CURRENT_PROFILE=$(curl -s http://localhost/profile)
   fi
 
-  if [ ${CURRENT_PROFILE} == real1 ]
+  if [[ ${CURRENT_PROFILE} == real1 ]];
   then
     IDLE_PROFILE=real2
   else
@@ -23,7 +23,7 @@ function find_idle_profile() {
 function find_idle_port() {
   IDLE_PROFILE=$(find_idle_profile)
 
-  if [ ${IDLE_PROFILE} == real1 ]
+  if [[ ${IDLE_PROFILE} == real1 ]];
   then
     echo "8081"
   else
